3a7a047c4d5cf858f2d1b27f2545751c046d34c8
Ricardo Sosa <rsosa.ramos@gmail.com>
2022-12-05 15:25:58 -0600

    Initial Commit

---
+++ src/app/models/empleado.ts
@@ -0,0 +1,15 @@
+export class Empleado {
+    archivo: number;
+    nombre: string;
+    apellido: string;
+    sexo: string;
+    salario: number;
+
+    constructor(archivo: number, nombre: string, apellido: string, sexo: string, salario: number;) {
+        this.archivo = archivo;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.sexo = sexo;
+        this.salario = salario;
+    }
+}
